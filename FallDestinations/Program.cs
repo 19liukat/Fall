24af6f06c65e46d5e2ce043313f8b54cb0ebadca
Rajat Mittal <19mittar@elmbrookstudents.org>
2018-06-13 11:14:19 -0500

    Adding my school in the fall

--- FallDestinations/Program.cs
+++ FallDestinations/Program.cs
@@ -9,6 +9,7 @@ namespace FallDestinations
             Console.WriteLine("Hello World!");
             Console.WriteLine("Brookfield Central - Reena");
             Console.WriteLine("Brett - Brookfield Central");
+            Console.WriteLine("Rajat-Brookfield Central");
             Console.ReadKey();
            
         }
